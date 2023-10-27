3e0d951e0761ea9b4e5434e8136753a827d7f0f5
hunsang <hoonsang99@korea.ac.kr>
2023-10-27 13:01:49 +0900

    Client side fuzzing

--- AFL_reverse/test_server.py
+++ AFL_reverse/test_server.py
@@ -2,6 +2,10 @@ import asyncio
 import websockets
 import socket
 
+sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock_server.bind(("127.0.0.1", 8181))
+sock_server.listen()
+client, _ = sock_server.accept()
 
 class Server:
     def __init__(self):
@@ -9,18 +13,17 @@ class Server:
         
 
     async def handler(self, websocket):
-        self.sock_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock_server.bind(("127.0.0.1", 8181))
-        self.sock_server.listen()
 
-        received_data = await websocket.recv()
+
+        received_data = websocket.recv()
         print("[-] Client2's requenst recieved") 
 
-        client, _ = self.sock_server.accept()
         request = client.recv(1024)
         print("[-]client1's requenst recieved")
         print(b"Sended: "+ request)
-        await websocket.send(request.decode())
+        websocket.send(request)
+        #websocket.send("Hi")
+
 
     def run(self):
         asyncio.get_event_loop().run_until_complete(self.ws_server)
